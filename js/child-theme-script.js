d50f7aedf5b44551ad375924f869fb193f8a646b
Abdulaha Islam <abdulahaislam210917@gmail.com>
2022-09-26 23:44:11 +0600

    terms-and-conditions

--- js/child-theme-script.js
+++ js/child-theme-script.js
@@ -5,6 +5,10 @@
 		jQuery(".thankyou-modal-close").click(function(){
 			jQuery(".thankyou-full-width-modal").toggleClass("close-modal");
 		});
+		
+		jQuery(".wc-terms-and-conditions").click(function(){
+			jQuery(".wc-terms-and-conditions").toggleClass("open");
+		});
 
 	});
 
